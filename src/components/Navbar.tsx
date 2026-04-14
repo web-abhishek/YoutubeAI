8127d9bde9263b4f2e8f43d8af2017d2e7c411ac
web-abhishek <web.abhiacharya@gmail.com>
2026-04-14 20:20:09 +0530

    Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,13 @@
+import { RxHamburgerMenu } from "react-icons/rx";
+import logo from "../assets/youtube-logo.png";
+
+
 const Navbar = () => {
     return (
         <section className="max-w-7xl mx-auto px-4 py-6">
             <nav className="flex items-center justify-between">
-                
+                <RxHamburgerMenu />
+                <img src={logo} alt="Youtube Logo" />
             </nav>
         </section>
     )
