a1a7949d39fdee76f9497ca425c62c052f67b634
web-abhishek <web.abhiacharya@gmail.com>
2026-04-16 17:17:09 +0530

    Navbar, Sidebar Created

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,31 @@
 import { RxHamburgerMenu } from "react-icons/rx";
 import logo from "../assets/youtube-logo.png";
-
+import { IoIosSearch } from "react-icons/io";
+import { FaRegUserCircle } from "react-icons/fa";
+import { BsThreeDotsVertical } from "react-icons/bs";
 
 const Navbar = () => {
+
     return (
-        <section className="max-w-7xl mx-auto px-4 py-6">
+        <section className="max-w-9xl mx-auto px-4 py-4 font-[roboto]">
             <nav className="flex items-center justify-between">
-                <RxHamburgerMenu />
-                <img src={logo} alt="Youtube Logo" />
+                <div className="flex items-center">
+                    <RxHamburgerMenu className="text-2xl" />
+                    <img src={logo} alt="Youtube Logo" className="w-36 pl-5" />
+                </div>
+                <div className="flex items-center">
+                    <input type="text" placeholder="Search" className="w-lg border border-gray-300 rounded-4xl rounded-tr-none rounded-br-none px-5 py-2 outline-none" />
+                    <button className="border border-gray-300 rounded-4xl rounded-tl-none rounded-bl-none px-5 py-2 cursor-pointer">
+                        <IoIosSearch className="text-2xl" />
+                    </button>
+                </div>
+                <div className="flex items-center">
+                    <BsThreeDotsVertical />
+                    <button className="text-blue-500 flex items-center border border-gray-300 shadow rounded-4xl px-5 py-2 ml-4 cursor-pointer">
+                        <FaRegUserCircle className="text-2xl pr-2 m-0 pb-0" />
+                        <span className="font-bold text-sm m-0 pb-0">Sign in</span>
+                    </button>
+                </div>
             </nav>
         </section>
     )
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,7 +1,40 @@
+import Home from "../assets/home.png";
+import Shorts from "../assets/shorts.png";
+import Subscriptions from "../assets/subscribe.png";
+import Library from "../assets/library.png";
+import History from "../assets/history.png";
+import WatchLater from "../assets/clock.png";
+import LikedVideos from "../assets/video.png";
+
+
 const Sidebar = () => {
     return (
-        <div>
-            Sidebar
+        <div className="w-64 px-4 py-6 font-[roboto]">
+            <ul className="space-y-2 border-b border-gray-300 pb-4">
+                <li className="bg-gray-200 px-4 py-2 rounded-xl font-semibold text-sm flex items-center cursor-pointer">
+                    <img src={Home} alt="" className="pr-4 w-8" />
+                    Home</li>
+                <li className="px-4 py-2 font-semibold text-sm flex items-center cursor-pointer">
+                    <img src={Shorts} alt="" className="pr-4 w-8" />
+                    Shorts</li>
+               <li className="px-4 py-2 font-semibold text-sm flex items-center cursor-pointer">
+                    <img src={Subscriptions} alt="" className="pr-4 w-8" />
+                    Subscriptions</li>
+            </ul>
+            <ul className="space-y-2">
+                <li className="px-4 py-2 font-semibold text-sm flex items-center cursor-pointer">
+                    <img src={Library} alt="" className="pr-4 w-8" />
+                    Library</li>
+                <li className="px-4 py-2 font-semibold text-sm flex items-center cursor-pointer">
+                    <img src={History} alt="" className="pr-4 w-8" />
+                    History</li>
+                <li className="px-4 py-2 font-semibold text-sm flex items-center cursor-pointer">
+                    <img src={WatchLater} alt="" className="pr-4 w-8" />
+                    Watch Later</li>
+                <li className="px-4 py-2 font-semibold text-sm flex items-center cursor-pointer">
+                    <img src={LikedVideos} alt="" className="pr-4 w-8" />
+                    Liked Videos</li>
+            </ul>
         </div>
     )
 }
