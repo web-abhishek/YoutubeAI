5b5fcc8a6c535b8adc06f246efbc6b6f7344ea83
web-abhishek <web.abhiacharya@gmail.com>
2026-04-17 20:34:10 +0530

    utils file

---
+++ src/utils/Constant.tsx
@@ -0,0 +1,4 @@
+const Google_API_KEY = "";
+
+const YOUTUBE_API_KEY =
+    `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key=${Google_API_KEY}`;
